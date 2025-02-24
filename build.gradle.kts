eb7db52a4ef773c9ad0532b6b482de461bb688f0
gdebenedetti <gdebenedetti@gmail.com>
2025-02-24 18:28:25 -0300

    feat: agregar Checkstyle para verificar estándares de código Java

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     // Define el plugin de Java para compilar proyectos en Java
     id("java")
+    // Plugin para verificar estándares de código Java
+    id("checkstyle")
 }
 
 repositories {
@@ -17,6 +19,7 @@ allprojects {
 
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "checkstyle")
 
     dependencies {
         // Dependencia de JUnit 5 para pruebas
@@ -26,5 +29,13 @@ subprojects {
     tasks.withType<Test> {
         // Configura las pruebas para usar JUnit Platform
         useJUnitPlatform()
+    }
+
+    // Configuración de Checkstyle
+    checkstyle {
+        toolVersion = "10.12.5"  // Versión de Checkstyle
+        configFile = rootProject.file("config/checkstyle/checkstyle.xml")
+        isIgnoreFailures = true  // No falla el build si hay errores de estilo
+        isShowViolations = true  // Muestra las violaciones en la consola
     }
 }
