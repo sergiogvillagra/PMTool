503c2dcfab433865d5ec6364d931a7013a4b6291
gdebenedetti <gdebenedetti@gmail.com>
2025-03-03 17:01:34 -0300

    chore: actualizar configuración del workflow y ajustar CucumberTest

--- app/src/test/java/org/pmtool/CucumberTest.java
+++ app/src/test/java/org/pmtool/CucumberTest.java
@@ -1,20 +1,16 @@
 package org.pmtool;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
-import org.junit.jupiter.api.Tag;
-
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
 @Suite
-@IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.pmtool.steps")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,json:build/reports/cucumber/cucumber.json")
-@Tag("cucumber")
 public class CucumberTest {
     // Esta clase no necesita métodos, solo sirve como punto de entrada para Cucumber
 } 
