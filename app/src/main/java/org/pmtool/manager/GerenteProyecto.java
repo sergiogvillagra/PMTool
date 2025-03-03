8db8a317cdc410949541895f66e44770744ccd67
gdebenedetti <gdebenedetti@gmail.com>
2025-03-03 16:29:15 -0300

    feat: finalizar proyecto

--- app/src/main/java/org/pmtool/manager/GerenteProyecto.java
+++ app/src/main/java/org/pmtool/manager/GerenteProyecto.java
@@ -5,8 +5,13 @@ import org.pmtool.model.Proyecto;
 import java.time.LocalDate;
 
 public class GerenteProyecto {
+    
     public void planificarProyecto(Proyecto proyecto, LocalDate inicio, LocalDate fin) {
         proyecto.planificarFechas(inicio, fin);
+    }
+
+    public void finalizarProyecto(Proyecto proyecto, LocalDate fechaFinReal) {
+        proyecto.finalizar(fechaFinReal);
     }
 
     public Proyecto.Actividad crearActividad(Proyecto proyecto, String numeroPadre, String nombre, int horas) {
